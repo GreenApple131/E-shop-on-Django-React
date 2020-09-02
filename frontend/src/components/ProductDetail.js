db79f420d255b4788c9301535441f6506e9f4aed
Dmytro Lutsiv <dimalg400@gmail.com>
2020-09-02 23:58:18 +0300

    detail item page

--- frontend/src/components/ProductDetail.js
+++ frontend/src/components/ProductDetail.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { 
-  Button, 
-  Container, 
-  Dimmer, 
-  Icon, 
-  Image, 
-  Item, 
-  Label, 
-  Loader, 
-  Message, 
-  Segment 
-} from 'semantic-ui-react';
-import { productDetailURL, addToCartURL } from '../constants';
-import { authAxios } from '../utils';
-import { fetchCart } from '../store/actions/cart';
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import {
+  Button,
+  Container,
+  Dimmer,
+  Icon,
+  Image,
+  Item,
+  Label,
+  Loader,
+  Message,
+  Segment
+} from 'semantic-ui-react'
+import { productDetailURL, addToCartURL } from '../constants'
+import { authAxios } from '../utils'
+import { fetchCart } from '../store/actions/cart'
 
 
 class ProductDetail extends Component {
@@ -27,28 +28,30 @@ class ProductDetail extends Component {
   }
 
 
-  async componentDidMount() {
+  componentDidMount() {
     this.handleFetchItem();
   }
 
   handleFetchItem = () => {
+    const { match: { params } } = this.props;
     this.setState({ loading: true });
     this.props.fetchCart(); // update the cart count
-    const res = await axios.get(productDetailURL())
-    .then(res => {
-      console.log(res.data);
-      this.setState({ data: res.data, loading: false });
-    })
-    .catch(err => {
-      this.setState({ error: err, loading: false });
-    })
+    axios
+      .get(productDetailURL(params.productID))
+      .then(res => {
+        console.log(res.data);
+        this.setState({ data: res.data, loading: false });
+      })
+      .catch(err => {
+        this.setState({ error: err, loading: false });
+      })
   }
 
-  
+
 
   handleAddToCart = slug => {
     this.setState({ loading: true });
-    
+
     authAxios
       .post(addToCartURL, { slug })
       .then(res => {
@@ -66,7 +69,7 @@ class ProductDetail extends Component {
 
   render() {
     const { data, error, loading } = this.state;
-    
+    const item = data;
     return (
       <Container>
         {error && ( // if error then do smth after &&
@@ -89,29 +92,28 @@ class ProductDetail extends Component {
           <h2 className="center">Our items</h2>
         </div>
         <Item.Group divided>
-          {this.state.data.map(item => {
 
-            return <Item key={item.id}>
-              <Item.Image src={item.image} />
-              <Item.Content>
-                <Item.Header as='a'>{item.title}</Item.Header>
-                <Item.Meta>
-                  <span className='cinema'>{item.category}</span>
-                </Item.Meta>
-                <Item.Description>{item.description}</Item.Description>
-                <Item.Extra>
-                  <Button primary floated='right' icon labelPosition='right' onClick={() => this.handleAddToCart(item.slug)}>
-                    Add to cart
-                    <Icon name='cart plus' />
-                  </Button>
-                  {item.discount_price && 
-                    <Label color={item.label === "primary" ? "blue": item.label === "secondary" ? "green": "olive"}>
-                      {item.label}
-                    </Label>}
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          })}
+          <Item key={item.id}>
+            <Item.Image src={item.image} />
+            <Item.Content>
+              <Item.Header as='a'>{item.title}</Item.Header>
+              <Item.Meta>
+                <span className='cinema'>{item.category}</span>
+              </Item.Meta>
+              <Item.Description>{item.description}</Item.Description>
+              <Item.Extra>
+                <Button primary floated='right' icon labelPosition='right' onClick={() => this.handleAddToCart(item.slug)}>
+                  Add to cart
+                  <Icon name='cart plus' />
+                </Button>
+                {item.discount_price &&
+                  <Label color={item.label === "primary" ? "blue" : item.label === "secondary" ? "green" : "olive"}>
+                    {item.label}
+                  </Label>}
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+
         </Item.Group>
       </Container>
     );
@@ -124,7 +126,8 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(ProductDetail);
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(ProductDetail));
