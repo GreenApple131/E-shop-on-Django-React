4117aab4af4239d6a575ad428421cec5616ce01b
Dmytro Lutsiv <dimalg400@gmail.com>
2020-10-21 19:30:55 +0300

    static files check

--- eshop/settings.py
+++ eshop/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'djanygo.template.context_processors.debug',
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
